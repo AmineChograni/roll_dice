3ae4d7323b4de2f4f0fa8d43aeb126adc447e26a
Amine CHOGRANI <aminechograni@gmail.com>
2023-12-22 10:57:29 +0100

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black26,
-        body: Center(
-          child: Text('Hello world!'),
+        //backgroundColor: Colors.black26,
+        body: Container(
+          //color: Colors.amber,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 32, 87, 206),
+                Color.fromARGB(255, 45, 32, 230),
+                Color.fromARGB(255, 219, 27, 210),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello world!'),
+          ),
         ),
       ),
     ),
