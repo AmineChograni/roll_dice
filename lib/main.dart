ba335063ff55245d824835668c912af58a304004
Amine CHOGRANI <aminechograni@gmail.com>
2023-12-22 11:23:09 +0100

    text style

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,31 @@ void main() {
             ),
           ),
           child: const Center(
-            child: Text('Hello world!'),
+            child: Text(
+              'Hello world!',
+              style: TextStyle(
+                color: Colors.white70,
+                //backgroundColor: Colors.amber,
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+                fontStyle: FontStyle.italic,
+                letterSpacing: -2,
+                wordSpacing: 8,
+                //height: 10,
+                shadows: [
+                  Shadow(
+                    color: Colors.amber,
+                    offset: Offset(5, 10),
+                    blurRadius: 10,
+                  ),
+                  Shadow(
+                    color: Color.fromARGB(255, 48, 47, 47),
+                    offset: Offset(-5, 10),
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
